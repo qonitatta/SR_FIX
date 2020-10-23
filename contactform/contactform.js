ab03b5829de36ef8439fb4a54b88b8e54b89880e
sillyatta UwU <qonitatta@gmail.com>
2020-10-23 14:04:06 +0700

    removing warning issues

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -32,7 +32,7 @@ jQuery(document).ready(function($) {
                         break;
 
                     case "minlen":
-                        if (i.val().length < parseInt(exp)) {
+                        if (i.val().length < parseInt(exp, 10)) {
                             ferror = ierror = true;
                         }
                         break;
